c8733d3fcf6023f3bac1648659def2d059eab9df
Kalichi Pictures <Kalichi@Kalichis-MacBook-Pro.local>
2024-10-10 16:39:49 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this is a script that demos typing annotation in functions
+"""
+
+
+def add(a: float, b: float) -> float:
+    """return a + b as floats"""
+    return a + b

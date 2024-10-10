21f75d9216b9648dbdcb71477fc3adf93d3768b6
Kalichi Pictures <Kalichi@Kalichis-MacBook-Pro.local>
2024-10-10 21:36:30 +0200

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pyhon3
+
+"""demo of type annotatio with tuples
+"""
+
+
+
+from typing import Union, Tuple
+import math
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns tuple from parameters"""
+
+    return str(k), v ** 2
